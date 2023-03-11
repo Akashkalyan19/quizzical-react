00faa03810018e50107c16bdf33e8fad5b17d641
akash <akshaykalyan7@gmail.com>
2023-03-11 15:52:03 +0530

    extra remove

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Start from "./Start.js"
 import Question from "./Question.js"
 
 export default function Quiz(props)
@@ -7,7 +6,6 @@ export default function Quiz(props)
     const [apiData,setApiData]=React.useState([])
     const [question,setQuestion]=React.useState()
     const [stage,setStage]=React.useState(1)
-    // const [disable,setDisable]=React.useState(0)
     
     React.useEffect(()=>{
         
@@ -45,116 +43,3 @@ export default function Quiz(props)
                     </section>
                 )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const [quizData,setQuizData]=React.useState()
-    // function getQuizData()
-    // {
-    //     const newQuiz=[]
-    //     // let obj={
-    //     //             quest:"",
-    //     //             cans:"",
-    //     //             ians:[] 
-    //     //          }
-    //     setQuizData(PrevData=>apiData.map(data=>{
-    //         console.log("setting data")
-    //         return{
-    //             quest: data.question,
-    //             cans: data.correct_answer,
-    //             ians: data.incorrect_answers 
-    //         }
-    //     }))
-    //         // for(let i=0;i<apiData.length;i++){
-    //         //                 obj.quest=apiData[i].question
-    //         //                 obj.cans=apiData[i].correct_answer
-    //         //                 obj.ians=apiData[i].incorrect_answers   
-    //         //                 newQuiz.push(obj)
-    //         //             }
-    //   return newQuiz   // console.log(newQuiz)
-    // }
